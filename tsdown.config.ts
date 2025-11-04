b02eda015fc09882165d7bba34ecdc9cf062f0cb
Seweryn Kras <seweryn.kras@golem.network>
2025-11-04 15:48:40 +0100

    add tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: "./src/**/index.ts",
+  format: ["esm", "cjs"],
+  exports: true, // override package.json exports field
+  platform: "neutral", // both browser and node
+});
