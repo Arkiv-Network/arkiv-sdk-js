066417988602b759b256fca5c8421eee55f7116c
Krzysztof Fonał <krzysiekfonal@gmail.com>
2025-11-04 20:20:03 +0100

    Fix building various distribution - esm, csj

--- src/utils/compression.ts
+++ src/utils/compression.ts
@@ -1,20 +1,44 @@
+// src/utils/compression.ts
 import type { BrotliWasmType } from "brotli-wasm"
-import brotliPromise from "brotli-wasm"
 
 let brotli: BrotliWasmType | null = null
-export async function compress(data: Uint8Array): Promise<Uint8Array> {
-  if (!brotli) {
-    brotli = await brotliPromise
+let brotliPromise: Promise<BrotliWasmType> | null = null
+
+async function getBrotli(): Promise<BrotliWasmType | null> {
+  if (brotli) return brotli
+
+  if (!brotliPromise) {
+    try {
+      const module = await import("brotli-wasm")
+      brotliPromise = Promise.resolve(module.default)
+      brotli = await brotliPromise
+    } catch (error) {
+      console.error("Error importing brotli-wasm", error)
+      brotli = require("brotli-wasm")
+    }
   }
+
+  brotli = await brotliPromise
+  return brotli
+}
+
+export async function compress(data: Uint8Array): Promise<Uint8Array> {
+  const brotliInstance = await getBrotli()
   console.debug(`Compressing data (size: ${data.length})`)
-  const brotliCompressed = brotli.compress(data)
+  if (!brotliInstance) {
+    console.warn("Brotli instance not found, not compressing data")
+    return data
+  }
+  const brotliCompressed = brotliInstance.compress(data)
   console.debug(`Brotli compressed data (size: ${brotliCompressed.length})`)
   return brotliCompressed
 }
 
 export async function decompress(data: Uint8Array): Promise<Uint8Array> {
-  if (!brotli) {
-    brotli = await brotliPromise
+  const brotliInstance = await getBrotli()
+  if (!brotliInstance) {
+    console.warn("Brotli instance not found, not decompressing data")
+    return data
   }
-  return brotli.decompress(data)
+  return brotliInstance.decompress(data)
 }
---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown"
+
+export default defineConfig({
+  entry: "./src/**/index.ts",
+  target: "esnext",
+  format: ["esm", "cjs"],
+  exports: true, // override package.json exports field
+  platform: "neutral", // both browser and node
+  external: [
+    // WASM packages should be external dependencies, not bundled
+    "brotli-wasm",
+  ],
+})
