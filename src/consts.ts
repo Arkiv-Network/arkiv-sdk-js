9ce0b9f98d977578c74c4fa5ec26b0f4293c9e23
CJ42 <cavallerajean@gmail.com>
2026-08-25 17:48:07 +0200

    docs: adjust comments to specify on entity events

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,9 @@
-
 /**
  * Address of the precompile registry contract.
- * The following actions can be done by calling this contract:
- * 
- * - Mutating entities by performing a CALL to this address.
- * - Fetching an entity nonce.
- * - Predicting the key of an entity prior to its creation.
+ * This address is used for:
+ * - Mutating entities by sending an entity mutation transaction (CALL) to this address.
+ * - Reading an entity nonce (used to predict future entity keys prior to creation).
+ * - Filtering/subscribing to entity events emitted from this address.
  */
 export const ARKIV_ADDRESS = "0x4400000000000000000000000000000000000044" as `0x${string}`
 export const BLOCK_TIME = 2 // 2 seconds
